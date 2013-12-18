075d1cb606afe8a4c364118ac18f052021bf40e8
Jad Kik <jadkik94@gmail.com>
2013-12-18 23:07:43 +0200

    Update setup.py with dependencies

--- setup.py
+++ setup.py
@@ -9,6 +9,8 @@ setup(
       
       namespace_packages=['cbpos', 'cbpos.mod'],
       include_package_data=True,
+
+      install_requires=['sqlalchemy>=0.7','PyDispatcher>=2.0.3','ProxyTypes>=0.9','PySide>=1.1.2'],
       
       author='Coinbox POS Team',
       author_email='coinboxpos@googlegroups.com',
